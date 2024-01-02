3fae10f52b5966fd98e13641d7bd0646a5a357c2
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-01 20:05:48 -0500

    Added math and seeking demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
 #include "raylib.h"
+#include "Math.h"
+
+Vector2 Seek(Vector2 target, Vector2 position, Vector2 velocity, float speed)
+{
+    return Normalize(target - position) * speed - velocity;
+}
 
 int main()
 {
@@ -7,11 +13,24 @@ int main()
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
     SetTargetFPS(60);
 
+    float radius = 25.0f;
+    Vector2 position{ screenWidth * 0.5f, screenHeight * 0.5f };
+    Vector2 velocity{ Random(-10.0f, 10.0f), Random(-10.0f, 10.0f) };
+
     while (!WindowShouldClose())
     {
+        float dt = GetFrameTime();
+        Vector2 mouse = GetMousePosition();
+        velocity = velocity + Seek(mouse, position, velocity, 1000.0f) * dt;
+        position = position + velocity * dt;
+
+        if (CheckCollisionPointCircle(mouse, position, radius))
+            break;
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Don't let the circle touch the cursor!", 10, 10, 20, LIGHTGRAY);
+        DrawCircleV(position, 25.0f, RED);
         EndDrawing();
     }
 
