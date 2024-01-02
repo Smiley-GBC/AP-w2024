78012b73b48973b52f48e8ab8d628386a55604b7
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-01 19:47:05 -0500

    Added raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,20 @@
+#include "raylib.h"
+
 int main()
 {
-	return 0;
+    const int screenWidth = 1280;
+    const int screenHeight = 720;
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
