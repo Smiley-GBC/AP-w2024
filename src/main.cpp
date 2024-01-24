6b856e89cb0ae000cdac49f04294325ada6789d1
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-24 09:34:13 -0500

    Made enemy static methods

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,9 @@
 #include "Math.h"
 #include <vector>   // Same as List<> in C#
 
+const int screenWidth = 1280;
+const int screenHeight = 720;
+
 // TODO -- make movement awesome with Seek behaviour
 struct Enemy
 {
@@ -9,6 +12,46 @@ struct Enemy
     float radius = 75.0f;
     Color color = RED;
     float speed = 300.0f;
+
+    static void Create()
+    {
+        // {} creates an empty object, so we're adding 4 empty enemies to our list
+        enemies.push_back({});
+        enemies.push_back({});
+        enemies.push_back({});
+        enemies.push_back({});
+
+        enemies[0].position = Vector2{ screenWidth * 0.1f, screenHeight * 0.1f };
+        enemies[1].position = Vector2{ screenWidth * 0.9f, screenHeight * 0.1f };
+        enemies[2].position = Vector2{ screenWidth * 0.9f, screenHeight * 0.9f };
+        enemies[3].position = Vector2{ screenWidth * 0.1f, screenHeight * 0.9f };
+
+        for (int i = 0; i < enemies.size(); i++)
+        {
+            // enemy.radius is the current enemy's radius. Enemy::baseRadius is the radius common to all enemies
+            enemies[i].radius = baseRadius + Random(-25.0f, 25.0f);
+
+            // enemy.speed is the current enemy's speed. Enemy::baseSpeed is the speed common to all enemies
+            enemies[i].speed = baseSpeed + Random(-250.0f, 250.0f);
+        }
+    }
+
+    static void Update(Vector2 targetPosition, float targetRadius, float dt)
+    {
+        for (int i = 0; i < enemies.size(); i++)
+        {
+            enemies[i].Move(targetPosition, targetRadius, dt);
+        }
+    }
+
+    static void Draw(unsigned char a)
+    {
+        for (int i = 0; i < enemies.size(); i++)
+        {
+            enemies[i].color.a = a;
+            DrawCircleV(enemies[i].position, enemies[i].radius, enemies[i].color);
+        }
+    }
 
     void Move(Vector2 targetPosition, float targetRadius, float dt)
     {
@@ -20,45 +63,38 @@ struct Enemy
         }
     }
 
-    // When used within an object, "static" means "common to all objects"
-    // So the value of baseRadius and baseSpeed will be shared across all enemies
+    // When used within an object, "static" means "common to all objects" (memory sharing).
+    // The value of baseRadius and baseSpeed will be shared across all enemies.
+
+    // When we say "static float n;" inside an object, we *declare* a static member
     static float baseRadius;
     static float baseSpeed;
+    static std::vector<Enemy> enemies;
 };
 
+// We still need to *define* each static member otherwise we get linker errors.
+// You can do so with the "scope-resolution" operator which is the ::
 float Enemy::baseRadius = 50.0f;
 float Enemy::baseSpeed = 250.0f;
+std::vector<Enemy> Enemy::enemies;
+
+// ie we have multiply baseRadius members so we "resolve the scope" of the class:
+//struct Player
+//{
+//    static float baseRadius;
+//};
+//
+//float Player::baseRadius = 50.0f;
 
 int main()
 {
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
     Vector2 centre{ screenWidth * 0.5f, screenHeight * 0.5f };
 
     Vector2 player = centre;
     float playerSpeed = 500.0f;
     float playerRadius = 50.0f;
 
-
-    std::vector<Enemy> enemies;
-    
-    // {} creates an empty object, so we're adding 4 empty enemies to our list
-    enemies.push_back({});
-    enemies.push_back({});
-    enemies.push_back({});
-    enemies.push_back({});
-
-    enemies[0].position = Vector2{ screenWidth * 0.1f, screenHeight * 0.1f };
-    enemies[1].position = Vector2{ screenWidth * 0.9f, screenHeight * 0.1f };
-    enemies[2].position = Vector2{ screenWidth * 0.9f, screenHeight * 0.9f };
-    enemies[3].position = Vector2{ screenWidth * 0.1f, screenHeight * 0.9f };
-
-    for (int i = 0; i < enemies.size(); i++)
-    {
-        enemies[i].radius = Enemy::baseRadius + Random(-25.0f, 25.0f);
-        enemies[i].speed = Enemy::baseSpeed + Random(-250.0f, 250.0f);
-    }
-
+    Enemy::Create();
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
     SetTargetFPS(60);
 
@@ -88,21 +124,11 @@ int main()
         }
         direction = Normalize(direction);
         player = player + direction * playerSpeed * dt;
-
-        for (int i = 0; i < enemies.size(); i++)
-        {
-            enemies[i].Move(player, playerRadius, dt);
-        }
+        Enemy::Update(player, playerRadius, dt);
 
         BeginDrawing();
         ClearBackground(GRAY);
-
-        for (int i = 0; i < enemies.size(); i++)
-        {
-            enemies[i].color.a = a;
-            DrawCircleV(enemies[i].position, enemies[i].radius, enemies[i].color);
-        }
-
+        Enemy::Draw(a);
         DrawCircleV(player, playerRadius, YELLOW);
         EndDrawing();
     }
