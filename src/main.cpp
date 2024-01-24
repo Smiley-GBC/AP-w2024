fc0c16dc2d1d626bea979c14dc9f1dbde7d1720d
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-24 09:12:06 -0500

    Game complete

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,6 @@
 #include <vector>   // Same as List<> in C#
 
 // TODO -- make movement awesome with Seek behaviour
-
 struct Enemy
 {
     Vector2 position{};
@@ -27,23 +26,23 @@ int main()
     const int screenWidth = 1280;
     const int screenHeight = 720;
     Vector2 centre{ screenWidth * 0.5f, screenHeight * 0.5f };
-    Vector2 enemy = centre;
+
     Vector2 player = centre;
     float playerSpeed = 500.0f;
     float playerRadius = 50.0f;
-    float enemySpeed = 300.0f;
-    float enemyRadius = 75.0f;
 
     std::vector<Enemy> enemies;
-    Enemy enemy1, enemy2, enemy3, enemy4;
-    enemy1.position = Vector2{ screenWidth * 0.1f, screenHeight * 0.1f };
-    enemy2.position = Vector2{ screenWidth * 0.9f, screenHeight * 0.1f };
-    enemy3.position = Vector2{ screenWidth * 0.9f, screenHeight * 0.9f };
-    enemy4.position = Vector2{ screenWidth * 0.1f, screenHeight * 0.9f };
-    enemies.push_back(enemy1);
-    enemies.push_back(enemy2);
-    enemies.push_back(enemy3);
-    enemies.push_back(enemy4);
+    
+    // {} creates an empty object, so we're adding 4 empty enemies to our list
+    enemies.push_back({});
+    enemies.push_back({});
+    enemies.push_back({});
+    enemies.push_back({});
+
+    enemies[0].position = Vector2{ screenWidth * 0.1f, screenHeight * 0.1f };
+    enemies[1].position = Vector2{ screenWidth * 0.9f, screenHeight * 0.1f };
+    enemies[2].position = Vector2{ screenWidth * 0.9f, screenHeight * 0.9f };
+    enemies[3].position = Vector2{ screenWidth * 0.1f, screenHeight * 0.9f };
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
     SetTargetFPS(60);
@@ -52,7 +51,6 @@ int main()
     {
         float tt = GetTime();
         float dt = GetFrameTime();
-        //float nsin = fabsf(sinf(tt));   // "normalized sin"
         float nsin = 1.0f - (sinf(tt) * 0.25f + 0.75f);
         unsigned char a = nsin * 255.0f;
 
@@ -76,15 +74,6 @@ int main()
         direction = Normalize(direction);
         player = player + direction * playerSpeed * dt;
 
-        bool collision = CheckCollisionCircles(enemy, enemyRadius * 3.0f, player, playerRadius);
-        Color playerColor = collision ? RED : YELLOW;
-        if (collision)
-        {
-            // AB = B - A
-            Vector2 toPlayer = Normalize(player - enemy);
-            enemy = enemy + toPlayer * enemySpeed * dt;
-        }
-
         for (int i = 0; i < enemies.size(); i++)
         {
             enemies[i].Move(player, playerRadius, dt);
@@ -99,15 +88,7 @@ int main()
             DrawCircleV(enemies[i].position, enemies[i].radius, enemies[i].color);
         }
 
-        DrawCircleV(enemy, enemyRadius, { 255, 0, 0, a });
-        DrawCircleV(player, playerRadius, playerColor);
-        // Proof of concept:
-        //unsigned char a1 = fabsf(sinf(tt)) * 255.0f;
-        //unsigned char a2 = (sinf(tt) * 0.5f + 0.5f) * 255.0f;
-        //Color enemy1 = { 255, 0, 0, a1 };
-        //Color enemy2 = { 255, 0, 0, a2 };
-        //DrawCircleV({ screenWidth * 0.25f, screenHeight * 0.5f }, 125.0f, enemy1);
-        //DrawCircleV({ screenWidth * 0.75f, screenHeight * 0.5f }, 125.0f, enemy2);
+        DrawCircleV(player, playerRadius, YELLOW);
         EndDrawing();
     }
 
