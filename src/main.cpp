015b4017cded307a4e3105b9f2434f78d67db168
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-24 08:52:53 -0500

    Added multiple enemy rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,39 @@
 #include "raylib.h"
 #include "Math.h"
+#include <vector>   // Same as List<> in C#
+
+// TODO -- make movement awesome with Seek behaviour
+
+struct Enemy
+{
+    Vector2 position{};
+    float radius = 75.0f;
+    Color color = RED;
+    float speed = 300.0f;
+};
 
 int main()
 {
     const int screenWidth = 1280;
     const int screenHeight = 720;
     Vector2 centre{ screenWidth * 0.5f, screenHeight * 0.5f };
+    Vector2 enemy = centre;
     Vector2 player = centre;
     float playerSpeed = 500.0f;
+    float playerRadius = 50.0f;
+    float enemySpeed = 300.0f;
+    float enemyRadius = 75.0f;
+
+    std::vector<Enemy> enemies;
+    Enemy enemy1, enemy2, enemy3, enemy4;
+    enemy1.position = Vector2{ screenWidth * 0.1f, screenHeight * 0.1f };
+    enemy2.position = Vector2{ screenWidth * 0.9f, screenHeight * 0.1f };
+    enemy3.position = Vector2{ screenWidth * 0.9f, screenHeight * 0.9f };
+    enemy4.position = Vector2{ screenWidth * 0.1f, screenHeight * 0.9f };
+    enemies.push_back(enemy1);
+    enemies.push_back(enemy2);
+    enemies.push_back(enemy3);
+    enemies.push_back(enemy4);
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
     SetTargetFPS(60);
@@ -16,7 +42,8 @@ int main()
     {
         float tt = GetTime();
         float dt = GetFrameTime();
-        float nsin = fabsf(sinf(tt));   // "normalized sin"
+        //float nsin = fabsf(sinf(tt));   // "normalized sin"
+        float nsin = 1.0f - (sinf(tt) * 0.25f + 0.75f);
         unsigned char a = nsin * 255.0f;
 
         Vector2 direction{};
@@ -39,11 +66,26 @@ int main()
         direction = Normalize(direction);
         player = player + direction * playerSpeed * dt;
 
+        bool collision = CheckCollisionCircles(enemy, enemyRadius * 3.0f, player, playerRadius);
+        Color playerColor = collision ? RED : YELLOW;
+        if (collision)
+        {
+            // AB = B - A
+            Vector2 toPlayer = Normalize(player - enemy);
+            enemy = enemy + toPlayer * enemySpeed * dt;
+        }
+
         BeginDrawing();
         ClearBackground(GRAY);
 
-        DrawCircleV(centre, 100.0f, { 255, 0, 0, a });
-        DrawCircleV(player, 100.0f, YELLOW);
+        for (int i = 0; i < enemies.size(); i++)
+        {
+            enemies[i].color.a = a;
+            DrawCircleV(enemies[i].position, enemies[i].radius, enemies[i].color);
+        }
+
+        DrawCircleV(enemy, enemyRadius, { 255, 0, 0, a });
+        DrawCircleV(player, playerRadius, playerColor);
         // Proof of concept:
         //unsigned char a1 = fabsf(sinf(tt)) * 255.0f;
         //unsigned char a2 = (sinf(tt) * 0.5f + 0.5f) * 255.0f;
