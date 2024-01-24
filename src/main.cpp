8d5173cf28bc3a6436a60119b0290d115313e589
Smiley98 <smileysofficialcinema@gmail.com>
2024-01-24 08:28:09 -0500

    Added game base

--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,56 @@
 #include "raylib.h"
 #include "Math.h"
 
-class Table
-{
-public:
-    float x, y;
-    float width, height;
-    float legWidth, legHeight;
-    float weight;
-    int legs;
-    Color legsColor;
-    Color topColor;
-
-    // Convert from function to method by removing the table parameter and refering to properties
-    void Draw()
-    {
-        float spacing = width / legs;
-        float legX = x;
-        for (int i = 0; i < legs; i++)
-        {
-            DrawRectangle(legX, y, legWidth, legHeight, legsColor);
-            legX += spacing;
-        }
-        DrawRectangle(x, y, width, height, topColor);
-    }
-};
-
-// Regular function
-//void Draw(Table table)
-//{
-//    DrawRectangle(table.x, table.y, table.width, table.height, table.color);
-//}
-
 int main()
 {
     const int screenWidth = 1280;
     const int screenHeight = 720;
-    Table woodTable;
-    woodTable.topColor = GRAY;
-    woodTable.legsColor = BROWN;
-    woodTable.height = 60.0f;
-    woodTable.width = 200.0f;
-    woodTable.x = screenWidth * 0.5f - woodTable.width * 0.5f;
-    woodTable.y = screenHeight * 0.75 - woodTable.height;
-    woodTable.legs = 4;
-    woodTable.legWidth = 10.0f;
-    woodTable.legHeight = woodTable.height * 3.0f;
-    
-    const char* sentence = "asdkjashdkjh";
+    Vector2 centre{ screenWidth * 0.5f, screenHeight * 0.5f };
+    Vector2 player = centre;
+    float playerSpeed = 500.0f;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
     SetTargetFPS(60);
 
     while (!WindowShouldClose())
     {
+        float tt = GetTime();
+        float dt = GetFrameTime();
+        float nsin = fabsf(sinf(tt));   // "normalized sin"
+        unsigned char a = nsin * 255.0f;
+
+        Vector2 direction{};
+        if (IsKeyDown(KEY_W))
+        {
+            direction.y = -1.0f;
+        }
+        if (IsKeyDown(KEY_S))
+        {
+            direction.y = 1.0f;
+        }
+        if (IsKeyDown(KEY_A))
+        {
+            direction.x = -1.0f;
+        }
+        if (IsKeyDown(KEY_D))
+        {
+            direction.x = 1.0f;
+        }
+        direction = Normalize(direction);
+        player = player + direction * playerSpeed * dt;
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText(sentence, screenWidth * 0.5f, screenHeight * 0.5f, 20, YELLOW);
-        woodTable.Draw();
+        ClearBackground(GRAY);
+
+        DrawCircleV(centre, 100.0f, { 255, 0, 0, a });
+        DrawCircleV(player, 100.0f, YELLOW);
+        // Proof of concept:
+        //unsigned char a1 = fabsf(sinf(tt)) * 255.0f;
+        //unsigned char a2 = (sinf(tt) * 0.5f + 0.5f) * 255.0f;
+        //Color enemy1 = { 255, 0, 0, a1 };
+        //Color enemy2 = { 255, 0, 0, a2 };
+        //DrawCircleV({ screenWidth * 0.25f, screenHeight * 0.5f }, 125.0f, enemy1);
+        //DrawCircleV({ screenWidth * 0.75f, screenHeight * 0.5f }, 125.0f, enemy2);
         EndDrawing();
     }
 
