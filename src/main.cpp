7004818cfb1bc75fc0fc90256efaf2f3da5c69a4
Ernie Burrows <114937798+ErnieBurrows@users.noreply.github.com>
2024-01-31 09:30:33 -0500

    Final Push
    oop implementation of "polymorphism"

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <raylib.h>
+//#include <raylib.h>
 #include <Math.h>
 #include <iostream>
 
@@ -6,6 +6,85 @@ using namespace std;
 
 //Pointer to a function that returns void and takes no arguments
 using DrawShape = void(*)();
+
+
+//int Add(int i, int i2){
+//	return i + i2;
+//}
+//
+//int Add(float i, float i2) {
+//	return i + i2;
+//}
+
+struct Shape
+{
+	virtual void Draw() = 0;
+
+	unsigned int vertexCount;
+	float area;
+	float length;
+};
+
+struct Circle : public Shape
+{
+	void Draw() final
+	{
+		cout << " #####" << endl;
+		cout << "#     #" << endl;
+		cout << "#     #" << endl;
+		cout << "#     #" << endl;
+		cout << " ##### " << endl;
+	}
+};
+
+struct Rectangle : public Shape
+{
+	void Draw() final
+	{
+		cout << "########" << endl;
+		cout << "#      #" << endl;
+		cout << "#      #" << endl;
+		cout << "#      #" << endl;
+		cout << "########" << endl;
+	}
+};
+
+struct Triangle : public Shape
+{
+	void Draw() final
+	{
+		cout << "    #   " << endl;
+		cout << "  #  #  " << endl;
+		cout << "#      #" << endl;
+		cout << "########" << endl;
+	}
+};
+
+void DrawManually()
+{
+	Shape* shape = nullptr;
+
+	shape = new Circle;
+	shape->Draw();
+	//(*shape).Draw(); //The same as shape->Draw()
+
+	shape = new Rectangle;
+	shape->Draw();
+
+	shape = new Triangle;
+	shape->Draw();
+}
+
+void DrawAutomatically()
+{
+	Shape* shape[] = { new Circle, new Rectangle, new Triangle };
+
+	for (int i = 0; i < 3; i++)
+	{
+		shape[i]->Draw();
+		delete shape[i];
+	}
+}
 
 void DrawCircle()
 {
@@ -33,36 +112,37 @@ void DrawTriangle()
 	cout << "########" << endl;
 }
 
-void DrawManually()
-{
-	DrawShape drawShape = nullptr;
-
-	drawShape = DrawCircle;
-	drawShape();
-
-	drawShape = DrawRectangle;
-	drawShape();
-
-	drawShape = DrawTriangle;
-	drawShape();
-}
-
-void DrawAutomatically()
-{
-	DrawShape shapes[] = { DrawCircle, DrawRectangle, DrawTriangle };
-
-	for (int i = 0; i < 3; i++) {
-		shapes[i]();
-	}
-}
+//void DrawManually()
+//{
+//	DrawShape drawShape = nullptr;
+//
+//	drawShape = DrawCircle;
+//	drawShape();
+//
+//	drawShape = DrawRectangle;
+//	drawShape();
+//
+//	drawShape = DrawTriangle;
+//	drawShape();
+//}
+//
+//void DrawAutomatically()
+//{
+//	DrawShape shapes[] = { DrawCircle, DrawRectangle, DrawTriangle };
+//
+//	for (int i = 0; i < 3; i++) {
+//		shapes[i]();
+//	}
+//}
 
 int main()
 {
 	cout << "Drawing manually. " << endl;
 	DrawManually();
 
-
+	
 	cout << "Drawing Automatically" << endl;
 	DrawAutomatically();
+
 	return 0;
 }
