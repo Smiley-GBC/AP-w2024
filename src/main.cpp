1b92d4efa0dd0fcf1247c31393c4781fecf34584
Smiley98 <smileysofficialcinema@gmail.com>
2024-02-05 10:56:07 -0500

    Static examples

--- src/main.cpp
+++ src/main.cpp
@@ -3,373 +3,71 @@
 #include <iostream>
 using namespace std;
 
-void PointerArithmetic();
-
-// Unless getters & setters do something meaningful, they shouldn't exist
-struct Circle1
+struct Enemy
 {
-    float GetRadius() { return radius; }
-    float SetRadius(float r) { radius = r; }
+    float health;
 
-private:
-    float radius;
+    // base health "declaration" --> tells the compiler "hey, base health is a thing"
+    static float baseHealth;
 };
 
-// This is a more "correct" version of the above code
-struct Circle2
+// base health "definition" --> tells the compiler specific information about base health
+// (without *specific information* you will get linker errors!!!)
+// :: is the "scope-resolution" operator. It resolves between different names
+float Enemy::baseHealth = 100.0f;
+
+struct Player
 {
-    float radius;
+    Player()
+    {
+        health = baseHealth;
+    }
+
+    float health;
+    static float baseHealth;
 };
 
-// Sometimes, its advantageous to make our code "immutable".
-// Meaning once our object has been created, it cannot be changed.
-// To achieve this, we set the radius ONCE on creation,
-// and make a getter but not a setter.
-struct Circle3
-{
-    Circle3(float r) { radius = r; }
-    float GetRadius() { return radius; }
-
-private:
-    float radius;
-};
-
-unsigned int Add(unsigned int a, unsigned int b)
-{
-    return a + b;
-}
-
-int Add(int a, int b)
-{
-    return a + b;
-}
-
-float Add(float a, float b)
-{
-    return a + b;
-}
-
-// Classes are the same as structs,
-// but they're private by default (whereas structs are public)
-class Test
-{
-public:
-    // This is a "constructor function". It runs *automatically* when an object is created
-    // If the constructor has no arguments, it is said to be the "default constructor"
-    // If the user doesn't overload the default constructor, the compiler will create one.
-    Test()
-    {
-        cout << "Test default constructor. . ." << endl;
-    }
-
-    // Here is an "overloaded constructor" that takes an integer argument.
-    // Just like functions, we can overload constructors!
-    Test(int integer)
-    {
-        value = integer;
-        cout << "Test overloaded int constructor " << integer << endl;
-    }
-
-    Test(float decimal)
-    {
-        value = decimal;
-        cout << "Test overloaded float constructor " << decimal << endl;
-    }
-
-    // This is called the "destructor" (indicated by '~').
-    // It runs when an object is destroyed!
-    ~Test()
-    {
-        cout << "Test " << value << " destroyed." << endl;
-    }
-
-    // Custom code that runs when we allocate Test with scalar-new
-    void* operator new(size_t size)
-    {
-        cout << "Overloading scalar new operator with size: " << size << endl;
-
-        // We know that Test has a size of 4 bytes since its only data is float value (float = 4 bytes)
-        // We must reserve 4 bytes of RAM. We do so by alloacting an untyped pointer (void*) via global new (::operator new(size)).
-        void* p = ::operator new(size);
-
-        // Keep track of allocation size
-        allocationBytes += size;
-
-        // Once we've alloacted memory, simply return it and interpret (implicit cast) it to Test!
-        return p;
-    }
-
-    // Custom code that runs when we deallocate Test with delete-new
-    void operator delete(void* p)
-    {
-        cout << "Overloading scalar delete operator " << endl;
-
-        // Subtract size of class on-deallocation
-        allocationBytes -= sizeof(Test);
-
-        ::delete p;
-    }
-
-    // Since the compiler differentiates between scalar new and array new[], we can overload it too!
-    void* operator new[](size_t size)
-    {
-        cout << "Overloading array new[] operator with size: " << size << endl;
-        void* p = ::operator new[](size);
-
-        // For array new and array delete, we don't know how many elements we're allocating/deallocating
-        // We'd have to do some math on the size we receive and encode it somehow.
-        // Might be a meaningful homework exercise to implement said tracking!
-        //allocationBytes += size;
-        return p;
-    }
-
-    // Since the compiler differentiates between scalar delete and array delete[], we can overload it too!
-    void operator delete[](void* p)
-    {
-        cout << "Overloading array delete[] operator " << endl;
-        delete[] p;
-    }
-
-    float value;
-
-public:
-    static int allocationBytes;
-};
-
-// This needs to be here otherwise we get linker errors
-int Test::allocationBytes;
-
-// Case 1: objects are destroyed because they've gone out of scope
-// This is because they're created on the "stack".
-// The stack is memory alloacted before the program runs by the compiler.
-// It is generally preferable because the compiler automatically deallocates the memory.
-// The downside is it is fixed-size, so we must know the amount of memory needed.
-// "The stack means the compiler does our baby-sitting for us :)"
-void StackConstructorDestructor()
-{
-    // Function overloading example (same name and argument count, different behaviour)
-    //cout << "Integer add: " << Add((int)1.75f, (int)2.5f) << endl;
-    //cout << "Decimal add: " << Add(1.75f, 2.5f) << endl;
-
-    // We can overload constructors just like functions.
-    // "Just because we *can* doesn't mean we *should*".
-    Test test;       // Outputs "Test default constructor. . ."
-    Test test1(1);   // Outputs "Test overloaded int constructor 1"
-    Test test2(1.5f);// Outputs "Test overloaded float constructor 1.5"
-
-    // Outputs "Test 1.5 destroyed."
-    // Outputs "Test 1 destroyed."
-    // This is because objects are destroyed in the opposite order they were created.
-    // (Has to do with the way they're allocated on the stack).
-}
-
-void HeapConstructorDestructor()
-{
-    // Initialize pointers to nullptr (address 0) for safety
-    Test* test1 = nullptr;
-    Test* test2 = nullptr;
-
-    // Dereferencing a null-pointer is allowed
-    //*test1;
-
-    // Reading from or writing to a null pointer is FORBIDDEN!!! (Crashes the program)
-    //cout << test2->value << endl;
-
-    // When allocating an object on the "heap" (basically somewhere in your ram),
-    // we use the "new" operator. This returns us a pointer to the object's memory.
-    test1 = new Test(1);
-    test2 = new Test(1.5f);
-
-    // "->" means "*(object.value)" aka same as '.' but automatically dereferenced
-    cout << test1->value << endl;
-    cout << test2->value << endl;
-
-    // When allocating with new, we must deallocate with delete.
-    // (We're the babysitter instead of the compiler).
-    delete test1;
-    delete test2;
-
-    // After deleting pointers, we want to invalidate them by assigning to nullptr.
-    // This will result in our program crashing instead of outputting garbage!
-
-    // Version 1 (bad):
-    //cout << test1->value << endl;
-    //cout << test2->value << endl;
-    //test1 = nullptr;
-    //test2 = nullptr;
-
-    // Version 2 (good):
-    //test1 = nullptr;
-    //test2 = nullptr;
-    //cout << test1->value << endl; // crash x1
-    //cout << test2->value << endl; // crash x2
-}
-
-void StackArray()
-{
-    Test tests[10];
-    for (int i = 0; i < 10; i++)
-    {
-        cout << tests[i].value << endl;
-    }
-    cout << endl;
-}
-
-void HeapArray()
-{
-    Test* tests = new Test[10];
-    for (int i = 0; i < 10; i++)
-    {
-        cout << tests[i].value << endl;
-    }
-
-    // If we allocate an array with "new[]", we must deallocate with "delete[]"
-    // Moreover, if we used "delete" instead of "delete[]", our program will crash
-    // since the memory reserves for new/delete vs new[]/delete[] are different!
-    // (new/delete = "scalar new" & "scalar delete", new[]/delete[] = "array new" & "array delete").
-    //delete tests;
-    delete[] tests;
-    cout << endl;
-}
-
-// Whenever we see "a + b", the compiler runs this function!
-float operator+(const Test& a, const Test& b)
-{
-    cout << "A + B: " << a.value + b.value << endl;
-    return a.value + b.value;
-}
-
-// Whenever we see "a - b", the compiler runs this function!
-float operator-(const Test& a, const Test& b)
-{
-    cout << "A - B: " << a.value - b.value << endl;
-    return a.value - b.value;
-}
-
-// Whenever we see "a * b", the compiler runs this function!
-float operator*(const Test& a, const Test& b)
-{
-    cout << "A * B: " << a.value * b.value << endl;
-    return a.value * b.value;
-}
-
-// Whenever we see "a / b", the compiler runs this function!
-float operator/(const Test& a, const Test& b)
-{
-    cout << "A / B: " << a.value / b.value << endl;
-    return a.value / b.value;
-}
-
-// We can do weird c++ things like overload
-// input ("stream extraction") and ouput ("stream insertion") operators:
-istream& operator>>(istream& in, Test& test/*non-constant reference cause we're modifying test*/)
-{
-    cout << "Enter the value of test: ";
-    in >> test.value;
-    return in;
-}
-
-ostream& operator<<(ostream& out, const Test& test/*constant reference cause test is read-only*/)
-{
-    out << "Test value: " << test.value << endl;
-    return out;
-}
-
-// Note that objects should be passed by-reference '&' to prevent destructor invocations.
-void MathOperators()
-{
-    // We're all familiar with basic math operators (+ - * /)
-    float n = 1.0f;
-    float m = 2.0f;
-    cout << n + m << endl;
-    cout << n - m << endl;
-    cout << n * m << endl;
-    cout << n / m << endl << endl;
-
-    // Since Test is a user-defined object (ADT), the compiler doesn't know how to add two tests.
-    // The compiler only knows how to do math on primitive (built-in, ie int, float etc) data-types!
-    // However, we can overload operators should we want this behaviour for our data-types.
-    Test a(1.0f);
-    Test b(2.0f);
-    a + b;
-    a - b;
-    a * b;
-    a / b;
-}
-
-void StreamOperators()
-{
-    float c;
-    cout << "Enter a decimal number: ";
-    cin >> c;
-    cout << "Decimal value: " << c << endl;
-
-    // Just like math, the compiler doesn't know how to input & output custom objects to/from streams.
-    // Hence, we must give it some help via operator overloading (stream insertion << and stream extraction >>)
-    Test d;
-    cin >> d;
-    cout << d;
-
-    // Generally << and >> aren't very useful. Don't worry if its weird and makes little sense.
-    // Focus on understanding the math operator overloads (+ - * /)
-}
-
-void MemoryOperators()
-{
-    // Calls Test::new, followed by Test's default constructor.
-    // This makes sense because we need to alloacte space for the object before we can construct the object!
-    
-    // "static" means "we don't need an instance to use it"
-    // static is used for general-purpose calculations since it can't rely on individual objects
-    cout << Test::allocationBytes << endl;
-    Test* a = new Test;
-    cout << Test::allocationBytes << endl;
-    Test* a1 = new Test;
-    cout << Test::allocationBytes << endl;
-    Test* a2 = new Test;
-    cout << Test::allocationBytes << endl;
-    Test* a3 = new Test;
-    cout << Test::allocationBytes << endl;
-
-    Test* b = new Test[2];
-
-    // Delete calls the destructor, and then deallocates the memory (opposite order of initialization).
-    delete a;
-    cout << Test::allocationBytes << endl;
-    delete a1;
-    cout << Test::allocationBytes << endl;
-    delete a2;
-    cout << Test::allocationBytes << endl;
-    delete a3;
-    cout << Test::allocationBytes << endl;
-
-    delete[] b;
-    cout << endl;
-}
+float Player::baseHealth;
 
 int main()
 {
-    // Signed vs unsigned function overloads
-    //unsigned int a1 = 1;
-    //unsigned int a2 = 2;
-    //int a3 = -3;
-    //int a4 = -4;
-    //Add(a1, a2);
-    //Add(a3, a4);
+    // Why :: is useful:
+    // We know we can't have duplicate variables because the compiler can't tell the difference.
+    //float baseHealth = 1.0f;
+    //float baseHealth = 2.0f;
 
-    // Pointer math examples ranging from automatic to manual
-    //PointerArithmetic();
+    // We use :: to "resolve the scope" of object-specific variables so that compiler knows who's who!
+    // (We need to tell the compiler which object baseHealth belongs to)
+    Player::baseHealth = 100.0f;
+    Enemy::baseHealth = 50.0f;
 
-    //StackConstructorDestructor();
-    //HeapConstructorDestructor();
+    Player player1;
+    cout << "Player 1 health: " << player1.health << endl;
 
-    //StackArray();
-    //HeapArray();
+    // Changes health for *ALL* players
+    cout << endl << "Changing base health to 200:" << endl;
+    Player::baseHealth = 200.0f;
+    Player player2;
+    Player player3;
+    cout << "Player 1 health: " << player1.health << endl;
+    cout << "Player 2 health: " << player2.health << endl;
+    cout << "Player 3 health: " << player3.health << endl;
 
-    //MathOperators();
-    //StreamOperators();
+    // Changes health for specific player
+    // (if we change baseHealth AFTER player1 is created, player1 is unaffected)
 
-    MemoryOperators();
+    cout << endl << "Multiplying player1's health by 5:" << endl;
+    player1.health *= 5.0f;
+    cout << "Player 1 health: " << player1.health << endl;
+
+    // All new players will now have a base health of 1000 (200 * 5)
+    cout << endl << "Changing base health to 1000:" << endl;
+    Player::baseHealth *= 5.0f;
+    Player player4;
+    cout << "Player 1 health: " << player1.health << endl;
+    cout << "Player 2 health: " << player2.health << endl;
+    cout << "Player 3 health: " << player3.health << endl;
+    cout << "Player 4 health: " << player4.health << endl;
 
     //const int screenWidth = 1280;
     //const int screenHeight = 720;
@@ -385,48 +83,4 @@ int main()
     //
     //CloseWindow();
     return 0;
-}
-
-void PointerArithmetic()
-{
-    int numbers[] = { 1, 2, 3, 4, 5 };
-    cout << numbers[0] << endl;
-    cout << numbers[1] << endl;
-    cout << numbers[2] << endl;
-    cout << numbers[3] << endl;
-    cout << numbers[4] << endl << endl;
-
-    // [] ("subscript operator") is shorthand to do pointer arithmetic and dereference
-    // The offset is a muliple of the datatype. ie n * sizeof(int)
-    // numbers + 0 means numbers + 0 * sizeof(int)
-    // numbers + 1 means numbers + 1 * sizeof(int)
-    // numbers + 2 means numbers + 2 * sizeof(int)
-    // numbers + 3 means numbers + 3 * sizeof(int)
-    // numbers + 4 means numbers + 4 * sizeof(int)
-    cout << *(numbers + 0) << endl;
-    cout << *(numbers + 1) << endl;
-    cout << *(numbers + 2) << endl;
-    cout << *(numbers + 3) << endl;
-    cout << *(numbers + 4) << endl << endl;
-
-    // We can prove this pointer arithmetic by doing the long-form:
-    int offset = sizeof(int);
-    cout << offset << endl << endl;
-
-    // Here we're saying "go to offset + 0 times size of integer"
-    char* start = reinterpret_cast<char*>(numbers);
-    cout << (int)*(start + 0 * offset) << endl;
-    cout << (int)*(start + 1 * offset) << endl;
-    cout << (int)*(start + 2 * offset) << endl;
-    cout << (int)*(start + 3 * offset) << endl;
-    cout << (int)*(start + 4 * offset) << endl;
-
-    // Again, we can use pointer arithmetic to iterate through an array!
-    // Points to numbers[0] (since numbers is just a pointer)
-    int* numberPtr = numbers;
-    cout << *numberPtr++ << endl;
-    cout << *numberPtr++ << endl;
-    cout << *numberPtr++ << endl;
-    cout << *numberPtr++ << endl;
-    cout << *numberPtr++ << endl;
 }
