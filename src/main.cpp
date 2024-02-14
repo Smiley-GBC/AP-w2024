2b19d8268efb26871acc790455eb7ef85e2cbb82
Smiley98 <smileysofficialcinema@gmail.com>
2024-02-14 09:19:15 -0500

    Added member-wise vs global operator overloads

--- src/main.cpp
+++ src/main.cpp
@@ -66,6 +66,28 @@ struct Vec2
 {
     float x;
     float y;
+
+    // Unary (1 argument) operator overload example
+    // Unary operators can only be "member-wise"
+    // (+=, -=, *=, /=, %= are all unary operators because they take 1 argument)
+    // (+, -, *, /, %, ==, != are all binary operators because they take 2 arguments)
+    Vec2& operator+=(Vec2 v)
+    {
+        cout << "Values before unary add [x: " << x << ", y: " << y << "]." << endl;
+        // Identical to above. "this" is just a pointer to the current object (Vec2*)
+        //cout << "Values before unary add [x: " << this->x << ", y: " << this->y << "]." << endl;
+        x += v.x;
+        y += v.y;
+        cout << "Values after unary add [x: " << x << ", y: " << y << "]." << endl;
+        return *this;
+    }
+
+    // Member-wise binary add
+    // (binary operators can be global OR member-wise whereas unary operators can only be member-wise)
+    Vec2 operator+(Vec2 b)
+    {
+        return { x + b.x, y + b.y };
+    }
 };
 
 struct Vec2Int
@@ -74,10 +96,11 @@ struct Vec2Int
     int y;
 };
 
-Vec2 operator+(Vec2 a, Vec2 b)
-{
-    return { a.x + b.x, a.y + b.y };
-}
+// Cannot exist at the same time as the member-wise binary add (line 87)
+//Vec2 operator+(Vec2 a, Vec2 b)
+//{
+//    return { a.x + b.x, a.y + b.y };
+//}
 
 Vec2Int operator+(Vec2Int a, Vec2Int b)
 {
@@ -97,22 +120,63 @@ vec2<T> operator+(vec2<T> a, vec2<T> b)
     return { a.x + b.x, a.y + b.y };
 }
 
-int main()
+template<typename T>
+struct Node
+{
+    T value = 0;
+    Node* next = nullptr;
+};
+
+void List()
+{
+    Node<float> n1;
+    n1.value = 1.1f;
+
+    Node<float> n2;
+    n2.value = 2.2f;
+
+    Node<float> n3;
+    n3.value = 3.3f;
+
+    Node<float> n4;
+    n4.value = 4.4f;
+
+    Node<float> n5;
+    n5.value = 5.5f;
+
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = &n4;
+    n4.next = &n5;
+
+    Node<float>* current = &n1;
+    while (current != nullptr)
+    {
+        cout << "Node value: " << current->value << endl;
+        current = current->next;
+    }
+}
+
+void TypeAdd()
+{
+    Vec2 a{ 1.5f, 2.5f };
+    Vec2 b{ 3.5f, 4.5f };
+    Vec2 c = a + b;
+    c += a;
+    cout << c.x << ' ' << c.y << endl;
+    
+    Vec2Int x{ 1.0f, 2.0f };
+    Vec2Int y{ 3.0f, 4.0f };
+    Vec2Int z = x + y;
+    cout << z.x << ' ' << z.y << endl;
+}
+
+void TemplateAdd()
 {
     // Compiler error because vec2 needs x & y members but Test only has a value member
     //vec2<Test> t1;
     //vec2<Test> t2;
     //vec2<Test> t3 = t1 + t2;
-
-    //Vec2 a{ 1.5f, 2.5f };
-    //Vec2 b{ 3.5f, 4.5f };
-    //Vec2 c = a + b;
-    //cout << c.x << ' ' << c.y << endl;
-    //
-    //Vec2Int x{ 1.0f, 2.0f };
-    //Vec2Int y{ 3.0f, 4.0f };
-    //Vec2Int z = x + y;
-    //cout << z.x << ' ' << z.y << endl;
 
     vec2<float> a{ 1.5f, 2.5f };
     vec2<float> b{ 3.5f, 4.5f };
@@ -123,7 +187,12 @@ int main()
     vec2<int> y{ 3, 4 };
     vec2<int> z = x + y;
     cout << z.x << ' ' << z.y << endl;
-        
+}
+
+int main()
+{
+    TypeAdd();
+
     // Single global instance of int
     //Singleton<int>::Instance()++;
     //Singleton<int>::Instance()++;
