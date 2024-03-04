173f32988e1cfe82f5fbc9e8dd0372df385ace97
Smiley98 <smileysofficialcinema@gmail.com>
2024-03-04 10:50:14 -0500

    Added weapon save/load

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
 #include <string>
 using namespace std;
 
-int main()
+void SimpleFileIO()
 {
 	const char* filePath = "File Test.txt";
 
@@ -28,6 +28,42 @@ int main()
 
 	inFile.close();
 	cout << fileContents;
+}
+
+struct Weapon
+{
+	string description;
+	int damage;
+	int type;
+	float range;
+	float weight;
+};
+
+int main()
+{
+	Weapon outWeapon, inWeapon;
+	outWeapon.description = "The mighty excalibur";
+	outWeapon.damage = 9999;
+	outWeapon.type = 420;
+	outWeapon.range = 10.0f;
+	outWeapon.weight = 25.0f;
+
+	const char* filePath = "Weapons.txt";
+	ofstream outFile;
+	outFile.open(filePath);
+	outFile
+		<< outWeapon.type << ' '
+		<< outWeapon.damage << ' '
+		<< outWeapon.range << ' '
+		<< outWeapon.weight << ' '
+		<< outWeapon.description << endl;
+	outFile.close();
+
+	ifstream inFile;
+	inFile.open(filePath);
+	inFile >> inWeapon.type >> inWeapon.damage >> inWeapon.range >> inWeapon.weight;
+	getline(inFile, inWeapon.description);
+	inFile.close();
 
 	return 0;
 }
