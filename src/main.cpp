4f716a9ec3d6474f7bc3768c174b6ac214575faf
Smiley98 <smileysofficialcinema@gmail.com>
2024-03-06 08:54:02 -0500

    Added neighbours

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include "raylib.h"
 #include "Math.h"
 #include <array>
+#include <queue>
+#include <vector>
 using namespace std;
 
 const float TILE_WIDTH = 80.0f;
@@ -12,6 +14,12 @@ struct Tile
     Vector2 position;
 };
 
+struct Cell
+{
+    int row;    // y
+    int col;    // x
+};
+
 enum TileType
 {
     GRASS,
@@ -20,6 +28,34 @@ enum TileType
     STONE,
     COUNT
 };
+
+vector<Cell> Neighbours(Cell cell, int rows, int cols)
+{
+    bool left = cell.col - 1 >= 0;
+    bool right = cell.col + 1 < cols;
+    bool top = cell.row - 1 >= 0;
+    bool bot = cell.row + 1 < rows;
+
+    vector<Cell> neighbours;
+    if (left) neighbours.push_back({ cell.row, cell.col - 1 });
+    if (right) neighbours.push_back({ cell.row, cell.col + 1 });
+    if (top) neighbours.push_back({ cell.row - 1, cell.col });
+    if (bot) neighbours.push_back({ cell.row + 1, cell.col });
+    return neighbours;
+}
+
+vector<Cell> FloodFill(Cell start, int rows, int cols)
+{
+    vector<Cell> cells;
+    //for (int row = 0; row < rows; row++)
+    //{
+    //    for (int col = 0; col < cols; col++)
+    //    {
+    //
+    //    }
+    //}
+    return cells;
+}
 
 int main()
 {
@@ -56,6 +92,14 @@ int main()
                 Color color = tileColors[grid[row][col]];
                 DrawRectangleV(position, { TILE_WIDTH, TILE_HEIGHT }, color);
             }
+        }
+
+        vector<Cell> neighbours = Neighbours({ 4, 4 }, TILE_COUNT, TILE_COUNT);
+        for (size_t i = 0; i < neighbours.size(); i++)
+        {
+            Cell neighbour = neighbours[i];
+            Vector2 position = Vector2{ neighbour.col * TILE_WIDTH, neighbour.row * TILE_HEIGHT };
+            DrawRectangleV(position, { TILE_WIDTH, TILE_HEIGHT }, PURPLE);
         }
 
         EndDrawing();
