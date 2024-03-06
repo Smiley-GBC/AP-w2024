19fc52e362c8f2dca0bb9db4afc1d9c449f7e270
Smiley98 <smileysofficialcinema@gmail.com>
2024-03-06 08:26:29 -0500

    Added 2d grid

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 #include <array>
 using namespace std;
 
-const float TILE_WIDTH = 60.0f;
+const float TILE_WIDTH = 40.0f;
 const float TILE_HEIGHT = 40.0f;
+const size_t TILE_COUNT = 20;
 
 struct Tile
 {
@@ -20,27 +21,34 @@ Color RandomColor()
 
 int main()
 {
-    const int screenWidth = 1280;
-    const int screenHeight = 720;
+    const int screenWidth = 800;
+    const int screenHeight = 800;
     InitWindow(screenWidth, screenHeight, "Tile Map");
     SetTargetFPS(60);
 
-    array<Tile, 10> tiles;
-    for (size_t i = 0; i < tiles.size(); i++)
+    array<array<Tile, TILE_COUNT>, TILE_COUNT> tiles;
+    for (size_t row = 0; row < TILE_COUNT; row++)
     {
-        tiles[i].position = Vector2{ i * TILE_WIDTH, 0.0f };
-        tiles[i].color = RandomColor();
+        for (size_t col = 0; col < TILE_COUNT; col++)
+        {
+            tiles[row][col].position = Vector2{ col * TILE_WIDTH, row * TILE_HEIGHT };
+            tiles[row][col].color = RandomColor();
+        }
     }
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        //DrawRectangle(0, 0, 60, 40, RED);
-        for (size_t i = 0; i < tiles.size(); i++)
+
+        for (size_t row = 0; row < TILE_COUNT; row++)
         {
-            DrawRectangleV(tiles[i].position, { TILE_WIDTH, TILE_HEIGHT }, tiles[i].color);
+            for (size_t col = 0; col < TILE_COUNT; col++)
+            {
+                DrawRectangleV(tiles[row][col].position, { TILE_WIDTH, TILE_HEIGHT }, tiles[row][col].color);
+            }
         }
+
         EndDrawing();
     }
 
