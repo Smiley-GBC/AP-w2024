76d6dac2a54ec50739978580d661ae64ae14aa12
Smiley98 <smileysofficialcinema@gmail.com>
2024-03-11 10:55:02 -0400

    Added rendering list

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,20 @@
 #include <iostream>
 using namespace std;
 
+constexpr int SCREEN_WIDTH = 800;
+constexpr int SCREEN_HEIGHT = 800;
+
+template<typename T>
 struct Node
 {
-	int data;
-	Node* next = nullptr;
+	T data;
+	Node<T>* next = nullptr;
 };
 
-int main()
+void NumberList()
 {
-	Node head;
-	Node number2, number3;
+	Node<int> head;
+	Node<int> number2, number3;
 	head.data = 1;
 	number2.data = 2;
 	number3.data = 3;
@@ -21,12 +25,49 @@ int main()
 	number2.next = &number3;
 
 	// Walk the list by starting at the head and advancing till there's no more connections
-	Node* current = &head;
+	Node<int>* current = &head;
 	while (current != nullptr)
 	{
 		cout << current->data << endl;
 		current = current->next;
 	}
+}
 
+int main()
+{
+	//NumberList();
+
+	Node< Vector2> p0;
+	Node< Vector2> p1;
+	Node< Vector2> p2;
+	Node< Vector2> p3;
+	p0.data = { SCREEN_WIDTH * 0.2f, SCREEN_HEIGHT * 0.2f };
+	p1.data = { SCREEN_WIDTH * 0.8f, SCREEN_HEIGHT * 0.2f };
+	p2.data = { SCREEN_WIDTH * 0.8f, SCREEN_HEIGHT * 0.8f };
+	p3.data = { SCREEN_WIDTH * 0.2f, SCREEN_HEIGHT * 0.8f };
+	p0.next = &p1;
+	p1.next = &p2;
+	p2.next = &p3;
+
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - basic window");
+	SetTargetFPS(60);
+	float r = 20.0f;
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		Node<Vector2>* current = &p0;
+		while (current != nullptr)
+		{
+			DrawCircleV(current->data, r, RED);
+			current = current->next;
+		}
+		//DrawCircleV(p0, r, RED);
+		//DrawCircleV(p1, r, RED);
+		//DrawCircleV(p2, r, RED);
+		//DrawCircleV(p3, r, RED);
+		EndDrawing();
+	}
+	CloseWindow();
 	return 0;
 }
