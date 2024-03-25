d77d7aba6284233b39a0a78993435ae132e812fe
Smiley98 <smileysofficialcinema@gmail.com>
2024-03-25 11:44:12 -0400

    Added queue enqueue

--- src/main.cpp
+++ src/main.cpp
@@ -70,7 +70,144 @@ private:
 	int count = 0;
 };
 
+// Dynamic stack - list implementation
+class DynamicStack
+{
+	// "Nested class" -- StackNode is only visible within DynamicStack
+private:
+	class StackNode
+	{
+		friend class DynamicStack;
+		int value;
+		StackNode* next;
+
+		// Constructor
+		StackNode(int value, StackNode* next = nullptr)
+		{
+			this->value = value;
+			this->next = next;
+		}
+	};
+
+	StackNode* top;
+public:
+	DynamicStack()
+	{
+		top = nullptr;
+	}
+
+	void Push(int value)
+	{
+		top = new StackNode(value, top);
+	}
+
+	int Pop()
+	{
+		assert(!IsEmpty());
+		int value = top->value;
+		StackNode* temp = top;
+		top = top->next;
+		delete temp;
+		return value;
+	}
+
+	bool IsEmpty() const
+	{
+		return (!top);
+	}
+};
+
+constexpr int QUEUE_MAX = 8;
+
+class Queue
+{
+public:
+	// Add a value to the back of our queue
+	void Enqueue(int value)
+	{
+		assert(!Full());
+
+		// Move back
+		back = (back + 1) % Capacity();
+
+		// Insert the new item
+		data[back] = value;
+	}
+
+	// Remove and return the front of our queue
+	int Dequeue()
+	{
+		assert(!Empty());
+
+		// Move front
+		front = (front + 1) % Capacity();
+
+		// Return the first item
+		return data[front];
+	}
+
+	// The value at the front of our queue (who is "first in line")
+	int Front()
+	{
+		return front;
+	}
+
+	int Back()
+	{
+		return back;
+	}
+
+	bool Empty()
+	{
+		return front == -1;
+	}
+
+	bool Full()
+	{
+		return Count() == Capacity();
+	}
+
+	int Count()
+	{
+		return back - front;
+	}
+
+	int Capacity()
+	{
+		return QUEUE_MAX;
+	}
+
+	void Print()
+	{
+		int i = front == -1 ? 0 : front;
+		int count = Count();
+		for (; i < count; i++)
+		{
+			//cout << i << endl;
+			cout << data[i] << endl;
+		}
+	}
+
+private:
+	int data[QUEUE_MAX];
+	int front = -1;
+	int back = -1;
+};
+
+void StackTest();
+
 int main()
+{
+	//StackTest();
+	Queue queue;
+	queue.Enqueue(3);
+	queue.Enqueue(6);
+	queue.Enqueue(9);
+	queue.Print();
+	return 0;
+}
+
+void StackTest()
 {
 	Stack<int, 8> integers;
 	cout << "Stack push" << endl;
@@ -100,6 +237,4 @@ int main()
 	decimals.Push(Random(-1.0f, 1.0f));
 	decimals.Push(Random(-1.0f, 1.0f));
 	decimals.Print();
-	
-	return 0;
 }
