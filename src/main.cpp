6dd8e4bdcb9b9129dcd805ec5a28c6fc8bca1a11
Smiley98 <smileysofficialcinema@gmail.com>
2024-03-25 10:26:25 -0400

    Added stack push & print

--- src/main.cpp
+++ src/main.cpp
@@ -4,399 +4,45 @@
 #include <string>
 #include <array>
 #include <vector>
+#include <cassert>
 using namespace std;
 
 constexpr int SCREEN_WIDTH = 800;
 constexpr int SCREEN_HEIGHT = 800;
 
-template<typename T>
-struct Node
-{
-	T data;
-	Node<T>* next = nullptr;
-};
+constexpr int STACK_MAX = 8;
 
-// "NodeFunc is a pointer to a function that returns void and accepts 1 integer argument"
-using NodeFunc = void(*)(int&);
-
-class List
+//template<typename T>
+class Stack
 {
 public:
-
-	// Destroy list by walking it and deleting each element
-	~List()
+	void Push(int value)
 	{
-		Clear();
+		// Ensure we have room in our stack
+		assert(count < STACK_MAX);
+		data[count] = value;
+		count++;
 	}
 
-	void Clear()
-	{
-		Node<int>* current = head;
-		while (current != nullptr)
-		{
-			Node<int>* temp = current;
-			current = current->next;
-			delete temp;
-		}
-		head = nullptr;
-	}
-
-	void Add(int number)
-	{
-		if (head == nullptr)
-		{
-			// Initialize our list if there's nothing in it
-			head = new Node<int>;
-			head->data = number;
-		}
-		else
-		{
-			// Loop until we find the last element
-			Node<int>* current = head;
-			while (current->next != nullptr)
-			{
-				current = current->next;
-			}
-
-			// Update the last element to point to the new node
-			Node<int>* node = new Node<int>;
-			current->next = node;
-			node->data = number;
-		}
-	}
-
-	void ForEach(NodeFunc each)
-	{
-		Node<int>* current = head;
-		while (current != nullptr)
-		{
-			each(current->data);
-			// Instead of printing, we can call our generic node function!
-			//cout << current->data << endl;
-			current = current->next;
-		}
-	}
-
-	void PrintRecursive(Node<int>* current)
-	{
-		// Base case: we've reached the end (node is nullptr)
-		if (current == nullptr)
-			return;
-
-		// Recursive case: print and advance
-		printf("Current node value: %i\n", current->data);
-		PrintRecursive(current->next);
-	}
-
-	// Print every number in our list by walking it and outputting the data of each node
 	void Print()
 	{
-		Node<int>* current = head;
-		while (current != nullptr)
-		{
-			cout << current->data << endl;
-			current = current->next;
-		}
+		for (int i = 0; i < count; i++)
+			cout << data[i] << endl;
 	}
 
-//private:
-	Node<int>* head = nullptr;
-};
-
-void NumberNodes()
-{
-	Node<int> head;
-	Node<int> number2, number3;
-	head.data = 1;
-	number2.data = 2;
-	number3.data = 3;
-
-	head.next = &number2;
-	number2.next = &number3;
-
-	// Walk the list by starting at the head and advancing till there's no more connections
-	Node<int>* current = &head;
-	while (current != nullptr)
-	{
-		cout << current->data << endl;
-		current = current->next;
-	}
-}
-
-void PrintNumber(int& number)
-{
-	printf("Current number is %i\n", number);
-}
-
-void TakeDamage(int& health)
-{
-	health -= 10;
-}
-
-void NumberList()
-{
-	List list;
-	list.Add(10);
-	list.Add(20);
-	list.Add(30);
-
-	// Display health before damage
-	list.ForEach(PrintNumber);
-
-	// Damage each enemy in the list
-	list.ForEach(TakeDamage);
-
-	// Display health after damage
-	list.ForEach(PrintNumber);
-
-	//list.PrintRecursive(list.head);
-	//list.Print();
-	//list.Clear();
-	//list.Print();
-	//list.Print();
-}
-
-void PathFollowing()
-{
-	// p = "point", r = "render"
-	Node< Vector2> p0, r0;
-	Node< Vector2> p1, r1;
-	Node< Vector2> p2, r2;
-	Node< Vector2> p3, r3;
-	p0.data = r0.data = { SCREEN_WIDTH * 0.2f, SCREEN_HEIGHT * 0.2f };
-	p1.data = r1.data = { SCREEN_WIDTH * 0.8f, SCREEN_HEIGHT * 0.2f };
-	p2.data = r2.data = { SCREEN_WIDTH * 0.8f, SCREEN_HEIGHT * 0.8f };
-	p3.data = r3.data = { SCREEN_WIDTH * 0.2f, SCREEN_HEIGHT * 0.8f };
-
-	// Path (p) end connects to start, whereas render (r) ends at 3
-	p0.next = &p1; r0.next = &r1;
-	p1.next = &p2; r1.next = &r2;
-	p2.next = &p3; r2.next = &r3;
-	p3.next = &p0;
-
-	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - basic window");
-	SetTargetFPS(60);
-	float r = 20.0f;
-
-	float time = 0.0f;
-	Node<Vector2>* a = &p0;	// a = start
-	Node<Vector2>* b = &p1;	// b = end
-	while (!WindowShouldClose())
-	{
-		float dt = GetFrameTime();
-		time += dt;
-		Vector2 c = Lerp(a->data, b->data, time);
-
-		// Change interval if close to the destination
-		if (Distance(b->data, c) < 10.0f)
-		{
-			time = 0.0f;
-			a = b;
-			b = b->next;
-		}
-
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-		Node<Vector2>* current = &r0;
-		while (current != nullptr)
-		{
-			DrawCircleV(current->data, r, RED);
-			current = current->next;
-		}
-		DrawCircleV(c, r, BLUE);
-		EndDrawing();
-	}
-	CloseWindow();
-}
-
-void Count(int number)
-{
-	// Recursive Case -- "condition for calling the function again"
-	if (number > 0)
-	{
-		printf("%i\n", number);
-		Count(number - 1);
-	}
-	else
-	// Base Case -- "condition to exit recursion"
-	{
-		printf("*End recursion*");
-	}
-}
-
-void InfiniteLoop()
-{
-	printf("RUN FOR YOUR LIFE IF YOU VALUE YOUR STACK POINTER!!!\n");
-	InfiniteLoop();
-}
-
-void NoMoreMemory()
-{
-	printf("Error 404: your PC can't run Cyrsis\n");
-}
-
-void MemoryGoofiness()
-{
-	// 1 byte (char) * 1024 = 1kb
-		// 1kb * 1024 = 1mb
-		// Our stack is less than 1mb (stack overflow if you run this)!
-	char data[1024 * 1024];
-
-	// Heap memory is essentially how much RAM you have (much larger than the stack)
-	// Allocate 1mb every iteration (may or may not crash your PC)
-	while (true)
-	{
-		char* memoryleak = new char[1024 * 1024];
-	}
-
+private:
+	int data[STACK_MAX];
 	int count = 0;
-	//std::set_new_handler(NoMoreMemory);
-	try {
-		int* pBigDataArray = new int[1000000000L];
-	}
-	catch (const std::bad_alloc&) {
-		NoMoreMemory();
-	}
-	for (int i = 0; i < 16; i++)
-	{
-		count = i + 1;
-		new char[1024 * 1024 * 1024];
-	}
-}
-
-int CharacterCount(char check, string text, int position)
-{
-	// Base case: we've reached the end of the string
-	if (position == text.length())
-		return 0;
-
-	// Recursive case 1: the current character is a match
-	if (text[position] == check)
-		return 1 + CharacterCount(check, text, position + 1);
-	else
-	// Recursive case 2: the current character is not a match
-		return CharacterCount(check, text, position + 1);
-}
-
-int Factorial(int n)
-{
-	if (n == 0)
-		return 1;
-	else
-		return n * Factorial(n - 1);
-	
-	// Short-hand for above. Returns 1 if true, otherwise factorial if false
-	//return n == 0 ? 1 : n * Factorial(n - 1);
-}
-
-int FactorialIterative(int n)
-{
-	int fact = 1;
-	for (int i = 1; i <= n; i++)
-		fact = fact * i;
-	return fact;
-}
-
-int Fibonacci(int n)
-{
-	if (n == 0)
-		return 0;
-	if (n == 1)
-		return 1;
-	return Fibonacci(n - 1) + Fibonacci(n - 2);
-}
-
-void FibonacciIterative(int num) {
-	// Instead of relying on recursion to compute previous 2 numbers,
-	// we simply store the previous 2 numbers in variables and update them!
-	int x = 0, y = 1, z = 0;
-	for (int i = 0; i < num; i++) {
-		cout << x << " ";
-		z = x + y;
-		x = y;
-		y = z;
-	}
-}
-
-void RecursiveNumbers()
-{
-	printf("%i\n", Factorial(6));
-	printf("%i\n", Fibonacci(0));	// 0
-	printf("%i\n", Fibonacci(1));	// 1
-	printf("%i\n", Fibonacci(2));	// 1
-	printf("%i\n", Fibonacci(3));	// 2
-	printf("%i\n", Fibonacci(4));	// 3
-	printf("%i\n", Fibonacci(5));	// 5
-	printf("%i\n", Fibonacci(6));	// 8
-}
-
-int LinearSearch(int number, vector<int> numbers)
-{
-	for (int i = 0; i < numbers.size(); i++)
-	{
-		if (numbers[i] == number)
-			return i;
-	}
-}
-
-// Homework: increase numbers to 100 and step through this with the debugger
-int BinarySearch(vector<int> numbers, int low, int high, int value)
-{
-	// Base case 1 (failure): we've reached the end of our array :(
-	if (low > high)
-		return -1;
-
-	// Valid midpoint if low < mid < high
-	int mid = (low + high) / 2;
-
-	// Base case 2 (success): stop searching if we've found our number :)
-	if (numbers[mid] == value)
-		return mid;
-
-	// Recursive case 1: if our guess is too low, search higher
-	if (numbers[mid] < value)
-		return BinarySearch(numbers, mid + 1, high, value);
-	
-	// Recursive case 2: if our guess is too high, search low
-	if (numbers[mid] > value)
-		return BinarySearch(numbers, low, mid - 1, value);
-}
-
-void Searches()
-{
-	vector<int> numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-	int number = 9;
-	//int position = LinearSearch(number, numbers);
-	int position = BinarySearch(numbers, 0, numbers.size() - 1, number);
-	printf("The position of %i is %i\n", number, position);
-}
-
-void Counting()
-{
-	printf("Iterative count:\n");
-	for (int i = 0; i < 5; i++)
-	{
-		printf("%i\n", i + 1);
-	}
-
-	printf("\nRecursive count:\n");
-	Count(5);
-}
+};
 
 int main()
 {
-	// Recursion stuff (week 11):
-	//InfiniteLoop();
-	//MemoryGoofiness();
-	//Counting();
-	//printf("%i\n", CharacterCount('r',"Connor", 0));
-	//RecursiveNumbers();
-	//Searches();
+	Stack numbers;
+	for (int i = 0; i < 8; i++)
+		numbers.Push(i);
 
-	// List stuff (week 10):
-	//NumberNodes();
-	NumberList();
-	//PathFollowing();
-	
+	// Don't exceed the capacity of our stack!
+	//numbers.Push(69);
+	numbers.Print();
 	return 0;
 }
