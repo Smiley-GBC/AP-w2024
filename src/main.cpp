123d37523182e89499b1fffbf7e8e8d767ae7520
Smiley98 <smileysofficialcinema@gmail.com>
2024-04-03 08:39:06 -0400

    Added player detection

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,26 @@ using namespace std;
 constexpr int SCREEN_WIDTH = 800;
 constexpr int SCREEN_HEIGHT = 800;
 
+constexpr float ENTITY_RADIUS = 25.0f;
+constexpr float WAYPOINT_RADIUS = 15.0f;
+
+// Basis for all game objects (Player & Enemy)
+struct Entity
+{
+	Vector2 position;
+	float angle;
+	float speed;
+};
+
 int main()
 {
+	Entity player;
+	player.position = Vector2{ SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+	player.speed = 250.0f;	// 250 pixels per second
+
+	Entity enemy;
+	float detectionRadius = 250.0f;
+
 	array<Vector2, 4> waypoints
 	{
 		Vector2{ SCREEN_WIDTH * 0.2f, SCREEN_HEIGHT * 0.2f },
@@ -21,19 +39,39 @@ int main()
 
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - basic window");
 	SetTargetFPS(60);
-	float r = 20.0f;
-	float time = 0.0f;
 
+	float time = 0.0f;
 	int current = 0;
 	int next = 1;
-
 	while (!WindowShouldClose())
 	{
 		float dt = GetFrameTime();
 		time += dt;
+
+		Vector2 direction = Vector2Zero();
+		if (IsKeyDown(KEY_W))
+		{
+			direction.y -= 1.0f;
+		}
+		if (IsKeyDown(KEY_S))
+		{
+			direction.y += 1.0f;
+		}
+		if (IsKeyDown(KEY_A))
+		{
+			direction.x -= 1.0f;
+		}
+		if (IsKeyDown(KEY_D))
+		{
+			direction.x += 1.0f;
+		}
+		direction = Normalize(direction);
+		player.position = player.position + direction * player.speed * dt;
+
 		Vector2 a = waypoints[current];
 		Vector2 b = waypoints[next];
 		Vector2 c = Lerp(a, b, time);
+		enemy.position = c;
 
 		// Change interval if close to the destination
 		if (Distance(b, c) < 10.0f)
@@ -43,11 +81,18 @@ int main()
 			++next %= waypoints.size();
 		}
 
+		bool playerDetected = CheckCollisionCircles(enemy.position, detectionRadius, player.position, ENTITY_RADIUS);
+		Color enemyColorFG = playerDetected ? RED : GREEN;
+		Color enemyColorBG = enemyColorFG;
+		enemyColorBG.a = 64;
+
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 		for (const Vector2& waypoint : waypoints)
-			DrawCircleV(waypoint, r, SKYBLUE);
-		DrawCircleV(c, r, BLUE);
+			DrawCircleV(waypoint, WAYPOINT_RADIUS, SKYBLUE);
+		DrawCircleV(enemy.position, detectionRadius, enemyColorBG);
+		DrawCircleV(enemy.position, ENTITY_RADIUS, enemyColorFG);
+		DrawCircleV(player.position, ENTITY_RADIUS, BLUE);
 		EndDrawing();
 	}
 	CloseWindow();
