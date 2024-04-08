ef4c67f23a6c7009baa91ab437bb04f445cf5c52
Smiley98 <smileysofficialcinema@gmail.com>
2024-04-08 11:31:58 -0400

    Failed attempt at bitwise layer masks

--- src/main.cpp
+++ src/main.cpp
@@ -1,312 +1,146 @@
 #include "raylib.h"
 #include "Math.h"
-
-#include "Stack.h"	// Custom LIFO
-#include "Queue.h"	// Custom FIFO
-
-#include <stack>	// Built-in "STL" LIFO
-#include <queue>	// Built-in "STL" FIFO
+#include <vector>
 
 #include <iostream>
 using namespace std;
 
-template<typename T>
-struct Node
+constexpr float SCREEN_WIDTH = 1280.0f;
+constexpr float SCREEN_HEIGHT = 720.0f;
+
+// Project circle-position onto line segment, then perform a point-circle collision check with the projected point!
+bool LineCircle(Vector2 lineStart, Vector2 lineEnd, Vector2 circlePosition, float circleRadius)
 {
-	T data;
-	Node<T>* next = nullptr;
+    Vector2 nearest = ProjectPointLine(lineStart, lineEnd, circlePosition);
+    return DistanceSqr(nearest, circlePosition) <= circleRadius * circleRadius;
+    // More optimal to compare squared distance instead of using a more expensive square-root calculation!
+}
+
+using byte = unsigned char;
+
+//constexpr int LAYER_PLAYER = 1;
+//constexpr int LAYER_TARGET = 2;
+//constexpr int LAYER_OBSTACLE = 3;
+//constexpr int LAYER_ALL = 4;
+
+// TODO -- figure out how to actually use bitmasking to achieve this xD
+constexpr int LAYER_ALL = 1;
+constexpr int LAYER_IGNORE_RAYCAST = 2;
+
+struct Entity
+{
+	Vector2 position;
+	Vector2 direction;
+	float radius;
+	int layer;
 };
 
-// "NodeFunc is a pointer to a function that returns void and accepts 1 integer argument"
-using NodeFunc = void(*)(int&);
-
-class List
+// If hit, write the position of the object to the hit parameter
+bool RayCast(const Entity& caster, const std::vector<Entity>& objects, Vector2& hitPosition, float viewDistance = FLT_MAX)
 {
-public:
+	Vector2 lineStart = caster.position;
+	Vector2 lineEnd = caster.position + caster.direction * viewDistance;
 
-	// Destroy list by walking it and deleting each element
-	~List()
+	std::vector<Vector2> hitPositions;
+
+	for (const Entity& entity : objects)
 	{
-		Clear();
+		bool hit = LineCircle(lineStart, lineEnd, entity.position, entity.radius);
+		bool layer = caster.layer == entity.layer;
+		//bool mask = caster.mask & entity.mask;
+		// Still have the same problem of testing the player against itself...
+		// The only point of layer masks is to support multiple layers by combining layers via bitwise!
+		if (hit && layer) hitPositions.push_back(entity.position);
 	}
 
-	void Clear()
+	size_t nearestIndex = 0;
+	float nearestDistance = FLT_MAX;
+	for (size_t i = 0; i < hitPositions.size(); i++)
 	{
-		Node<int>* current = head;
-		while (current != nullptr)
+		float objectDistance = Distance(caster.position, hitPositions[i]);
+		if (objectDistance < nearestDistance)
 		{
-			Node<int>* temp = current;
-			current = current->next;
-			delete temp;
-		}
-		head = nullptr;
-	}
-
-	void Add(int number)
-	{
-		if (head == nullptr)
-		{
-			// Initialize our list if there's nothing in it
-			head = new Node<int>;
-			head->data = number;
-		}
-		else
-		{
-			// Loop until we find the last element
-			Node<int>* current = head;
-			while (current->next != nullptr)
-			{
-				current = current->next;
-			}
-
-			// Update the last element to point to the new node
-			Node<int>* node = new Node<int>;
-			current->next = node;
-			node->data = number;
+			nearestDistance = objectDistance;
+			nearestIndex = i;
 		}
 	}
 
-	void AddSorted(int number)
-	{
-		if (head == nullptr || head->data >= number)
-		{
-			Node<int>* temp = head;
-			head = new Node<int>;
-			head->data = number;
-			head->next = temp;
-		}
-		else
-		{
-			Node<int>* previous = head;
-			Node<int>* current = head->next;
-
-			// Loop until we've reached the end or found our number
-			while (current != nullptr && current->data < number)
-			{
-				previous = current;
-				current = current->next;
-			}
-
-			// Insert sorted node in-between previous & next
-			Node<int>* node = new Node<int>;
-			node->data = number;
-			previous->next = node;
-			node->next = current;
-		}
-	}
-
-	// TODO 1 -- create a Get function
-	// TODO 2 -- modify the Remove method to output the removed value
-	void Remove(int number)
-	{
-		// Case 1: nothing in the list
-		if (head == nullptr)
-			return;
-
-		// Case 2: first element in list (head) is the element we want to remove
-		if (head->data == number)
-		{
-			Node<int>* next = head->next;
-			delete head;
-			head = next;
-		}
-
-		// Case 3: need to make previous element point to element after the one we want to remove
-		Node<int>* current = head;
-		Node<int>* previous = nullptr;
-		
-		// Loop until we've reached the end or found our number
-		while (current != nullptr && current->data != number)
-		{
-			previous = current;
-			current = current->next;
-		}
-
-		// Once our loop exits, if current is non-null, we've found the number!
-		// (Otherwise if current is null we've searched the entire list without luck)
-		if (current != nullptr)
-		{
-			previous->next = current->next;
-			delete current;
-		}
-	}
-
-	void ForEach(NodeFunc each)
-	{
-		Node<int>* current = head;
-		while (current != nullptr)
-		{
-			each(current->data);
-			// Instead of printing, we can call our generic node function!
-			//cout << current->data << endl;
-			current = current->next;
-		}
-	}
-
-	void PrintRecursive(Node<int>* current)
-	{
-		// Base case: we've reached the end (node is nullptr)
-		if (current == nullptr)
-			return;
-
-		// Recursive case: print and advance
-		printf("Current node value: %i\n", current->data);
-		PrintRecursive(current->next);
-	}
-
-	// Print every number in our list by walking it and outputting the data of each node
-	void Print()
-	{
-		Node<int>* current = head;
-		while (current != nullptr)
-		{
-			cout << current->data << endl;
-			current = current->next;
-		}
-	}
-
-private:
-	Node<int>* head = nullptr;
-};
-
-void StackTest();
-void QueueTest();
-void StackSTL();
-void QueueSTL();
+	if (!hitPositions.empty()) hitPosition = hitPositions[nearestIndex];
+	return !hitPositions.empty();
+}
 
 int main()
 {
-	List list;
-	list.AddSorted(3);
-	list.AddSorted(2);
-	list.AddSorted(1);
-	list.Print();
+	// The memory window uses "hexadecimal" which is base 16.
+	// This is convenient because 16 is a multiple of 2, so its easy to map to binary.
+	// Hexadecimal counts 0 1 2 3 4 5 6 7 8 9 A B C D E F where F == 15 in decimal.
+	
+	// Example of how to multiply by 2 via bit-shifting
+	byte bytes[8];
+	for (int i = 0; i < 8; i++)
+	{
+		byte digit = 0b00000001;
+		bytes[i] = digit << i;
+		// Uncomment to revert
+		//bytes[i] >>= i;
+	}
 
-	list.Remove(2);
-	list.Print();
+	byte hex[16]
+	{
+		0b00000000, 0b00000001, 0b00000010, 0b00000011,
+		0b00000100, 0b00000101, 0b00000110, 0b00000111,
+		0b00001000, 0b00001001, 0b00001010, 0b00001011,
+		0b00001100, 0b00001101, 0b00001110, 0b00001111,
+	};
 
-	//cout << "Custom stack test:" << endl;
-	//StackTest();
-	//
-	//cout << endl << "Custom queue test:" << endl;
-	//QueueTest();
-	//
-	//cout << endl << "STL stack test:" << endl;
-	//StackSTL();
-	//
-	//cout << endl << "STL queue test:" << endl;
-	//QueueSTL();
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game");
+    SetTargetFPS(60);
 
+	float obstacleRadius = 100.0f;
+	float playerRadius = 10.0f;
+	float targetRadius = 25.0f;
+	float rayLength = 750.0f;
+
+	Entity player, target, obstacle;
+	player.position = { SCREEN_WIDTH * 0.25f, SCREEN_HEIGHT * 0.75f };
+	target.position = { SCREEN_WIDTH * 0.75f, SCREEN_HEIGHT * 0.25f };
+	obstacle.position = { SCREEN_WIDTH * 0.5f, SCREEN_HEIGHT * 0.5f };
+
+	player.radius = 10.0f;
+	target.radius = 25.0f;
+	obstacle.radius = 100.0f;
+
+	// By default, everything can get hit with a raycast
+	player.layer = LAYER_IGNORE_RAYCAST;
+	target.layer = LAYER_ALL;
+	obstacle.layer = LAYER_ALL;
+
+	vector<Entity> objects;
+	objects.push_back(player);
+	objects.push_back(target);
+	objects.push_back(obstacle);
+
+    while (!WindowShouldClose())
+    {
+        // AB = B - A
+        player.direction = Normalize(GetMousePosition() - player.position);
+        //player.direction = Normalize(target.position - player.position);
+        Vector2 lineStart = player.position;
+        Vector2 lineEnd = player.position + player.direction * rayLength;
+
+		Vector2 hit{};
+		RayCast(player, objects, hit, rayLength);
+
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawCircleV(player.position, player.radius, GREEN);
+        DrawCircleV(target.position, target.radius, RED);
+        DrawCircleV(obstacle.position, obstacle.radius, GRAY);
+        DrawLineEx(lineStart, lineEnd, 5.0f, BLUE);
+		DrawText(TextFormat("x: %f y: %f", hit.x, hit.y), 10, 10, 20, RED);
+        EndDrawing();
+    }
+
+    CloseWindow();
 	return 0;
-}
-
-void StackTest()
-{
-	Stack<int, 8> integers;
-	cout << "Stack push" << endl;
-	for (int i = 0; i < 8; i++)
-		integers.Push(i);
-	integers.Print();
-
-	cout << "Stack pop" << endl;
-	for (int i = 0; i < 8; i++)
-		cout << integers.Pop() << endl;
-
-	if (integers.Empty())
-	{
-		cout << "No more numbers :(" << endl;
-	}
-
-	// Don't exceed the capacity of our stack!
-	if (!integers.Full())
-	{
-		integers.Push(69);
-	}
-	integers.Print();
-
-	Stack<float, 4> decimals;
-	decimals.Push(Random(-1.0f, 1.0f));
-	decimals.Push(Random(-1.0f, 1.0f));
-	decimals.Push(Random(-1.0f, 1.0f));
-	decimals.Push(Random(-1.0f, 1.0f));
-	decimals.Print();
-}
-
-void QueueTest()
-{
-	Queue<int, 8> queue;
-	queue.Enqueue(3);
-	queue.Enqueue(6);
-	queue.Enqueue(9);
-	queue.Enqueue(12);
-
-	for (int i = 0; i < 4; i++)
-	{
-		queue.Enqueue(i);
-	}
-	// 8 elements in queue --> back == [7], front = 0 (because nothing's been removed)
-
-	for (int i = 0; i < 3; i++)
-	{
-		queue.Dequeue();
-	}
-	// 5 elements in queue --> back == [7], front = 3 
-
-	queue.Print();
-
-	cout << "Queue wrap:" << endl;
-	for (int i = 0; i < 3; i++)
-	{
-		queue.Enqueue(i * 3);
-	}
-
-	queue.Print();
-	//cout << "Expecting 3, 6, 9:" << endl;
-	//queue.Print();
-	//
-	//cout << "Expecting 6, 9:" << endl;
-	//queue.Dequeue();
-	//queue.Print();
-	//
-	//cout << "Expecting 9:" << endl;
-	//queue.Dequeue();
-	//queue.Print();
-	//
-	//cout << "Expecting nothing!" << endl;
-	//queue.Dequeue();
-	//queue.Print();
-}
-
-void StackSTL()
-{
-	stack<int> stack;
-	stack.push(1);
-	stack.push(2);
-	stack.push(3);
-
-	while (!stack.empty())
-	{
-		// Get most recently added
-		cout << stack.top() << endl;
-
-		// Remove most recently added
-		stack.pop();
-	}
-}
-
-void QueueSTL()
-{
-	queue<int> queue;
-	queue.push(1);
-	queue.push(2);
-	queue.push(3);
-
-	while (!queue.empty())
-	{
-		// Get first in line
-		cout << queue.front() << endl;
-
-		// Remove first in line
-		queue.pop();
-	}
 }
